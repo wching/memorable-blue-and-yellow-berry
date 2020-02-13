7d386814398842771769df9ee2f9ad23357cb7a3
Walter Ching <walter.ching.b@gmail.com>
2020-02-13 20:50:06 +0100

    Add return type to the searchImages method

--- remote-flickr-client/src/main/java/com/ching/walter/remoteflickrclient/IFlickrApiService.kt
+++ remote-flickr-client/src/main/java/com/ching/walter/remoteflickrclient/IFlickrApiService.kt
@@ -1,5 +1,7 @@
 package com.ching.walter.remoteflickrclient
 
+import com.ching.walter.remoteflickrclient.features.search.model.response.RemoteFlickrSearchResponse
+import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -12,5 +14,5 @@ interface IFlickrApiService {
         @Query(value = "method") method: String,
         @Query(value = "nojsoncallback") noJsonCallback: String,
         @Query(value = "text") search: String
-    )
+    ): Observable<RemoteFlickrSearchResponse>
 }
