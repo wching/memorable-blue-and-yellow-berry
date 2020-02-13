2c7e1bbdb5d2dd06de0bfd329eab3a93f0159aed
Walter Ching <walter.ching.b@gmail.com>
2020-02-13 20:59:26 +0100

    Create FlickrApiController

---
+++ remote-flickr-client/src/main/java/com/ching/walter/remoteflickrclient/base/FlickrApiController.kt
@@ -0,0 +1,29 @@
+package com.ching.walter.remoteflickrclient.base
+
+import com.ching.walter.remoteflickrclient.IFlickrApiService
+import com.ching.walter.remoteflickrclient.base.RemoteFlickrConstants.API_KEY
+import com.ching.walter.remoteflickrclient.base.RemoteFlickrConstants.JSON_CALLBACK
+import com.ching.walter.remoteflickrclient.base.RemoteFlickrConstants.RESPONSE_FORMAT
+import com.ching.walter.remoteflickrclient.base.RemoteFlickrConstants.SEARCH_METHOD
+import com.ching.walter.remoteflickrclient.features.search.model.request.RemoteFlickrSearchParams
+import com.ching.walter.remoteflickrclient.features.search.model.response.RemoteFlickrSearchResponse
+import io.reactivex.Observable
+import javax.inject.Inject
+
+class FlickrApiController @Inject constructor(
+    private val flickrApiService: IFlickrApiService
+) {
+
+    fun searchImages(
+        remoteFlickrSearchParams: RemoteFlickrSearchParams
+    ): Observable<RemoteFlickrSearchResponse> =
+        with(remoteFlickrSearchParams) {
+            flickrApiService.searchImages(
+                apiKey = API_KEY,
+                format = RESPONSE_FORMAT,
+                method = SEARCH_METHOD,
+                noJsonCallback = JSON_CALLBACK,
+                search = search
+            )
+        }
+}
